e2a6280a55fac12ff2069587cd88cadcab60da6f
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-06-17 21:34:01 +0545

    Update place_model.dart

--- lib/models/place_model.dart
+++ lib/models/place_model.dart
@@ -3,12 +3,15 @@ class Place {
   final String city;
   final String country;
   final int properties;
+  final int time;
 
   Place(
       {required this.imageUrl,
       required this.city,
       required this.country,
-      required this.properties});
+      required this.properties,
+      required this.time,
+      });
 }
 
 final places = [
@@ -17,35 +20,41 @@ final places = [
     city: 'Amsterdam',
     country: 'Netherlands',
     properties: 203,
+    time: 7,
   ),
   Place(
     imageUrl: 'assets/images/bangkok.jpg',
     city: 'Bangkok',
     country: 'Thailand',
     properties: 301,
+    time: 9,
   ),
   Place(
     imageUrl: 'assets/images/london.jpg',
     city: 'London',
     country: 'United Kingdom',
     properties: 310,
+    time: 12,
   ),
   Place(
     imageUrl: 'assets/images/rome.jpg',
     city: 'Rome',
     country: 'Italy',
     properties: 235,
+    time: 8,
   ),
   Place(
     imageUrl: 'assets/images/sydney.jpg',
     city: 'Sydney',
     country: 'Australia',
     properties: 159,
+    time: 6,
   ),
   Place(
     imageUrl: 'assets/images/tokyo.jpg',
     city: 'Tokyo',
     country: 'Japan',
     properties: 253,
+    time: 7,
   ),
 ];
